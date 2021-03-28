8a148017e1671f31ee888682b5163521025ee20a
rothmil1 <rothmil1@students.zhaw.ch>
2021-03-28 16:47:05 +0200

    add more flask routes + CORS

--- app.py
+++ app.py
@@ -1,6 +1,31 @@
-from flask import Flask
+from flask import Flask, request
+from flask_cors import CORS, cross_origin
+
 app = Flask(__name__)
+cors = CORS(app)
+
+app.config['CORS_HEADERS'] = 'Content-Type'
+
 
 @app.route('/')
+@cross_origin()
 def hello_world():
-    return 'Hello, World! This app is deployed on heroku!'
+    print('[/] REQUEST', request)
+    return 'Hello, World! This app is deployed on heroku! 1'
+
+
+@app.route('/getReq', methods=['GET'])
+@cross_origin()
+def getReq():
+    print('[/getReq] REQUEST', request)
+    return {
+        'gurke': 'asd',
+        'age': 28
+    }
+
+
+@app.route('/ping', methods=['POST'])
+@cross_origin()
+def postPing():
+    print('[/ping] REQUEST', request)
+    return request.data
